b647da6f0ce7e3af40f7f7cf0e990850b6ce1de7
Client9@EST.LAN <Client9@EST.LAN>
2019-11-05 16:45:36 +0000

    question 1&2&3&4&5&6

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const SCORE_MAX=100;
-const joueurActif = 0;
+//bouhnach fatima zohra // Dalle sanaa // pc16
+const SCORE_MAX=30;
+var joueurActif = 0;
 const activeJ1 = document.querySelector(".panel-joueur-0");
 const activeJ2 = document.querySelector(".panel-joueur-1");
 const NomJoueur1= document.querySelector("#nom-0");
 
-const score1= document.querySelector("#score-0");
+var score1= document.querySelector("#score-0");
 const NomJoueur2= document.querySelector("#nom-1");
-const score2= document.querySelector("#score-1");
+var score2= document.querySelector("#score-1");
 const image= document.querySelector(".de");
 const tempScore1=document.querySelector("#courent-0");
 const tempScore2=document.querySelector("#courent-1");
@@ -18,9 +19,10 @@ const btnPasse=document.querySelector(".btn-passe");
 
 btnNew.addEventListener("click",init,false);
 btnLancer.addEventListener("click",lancer,false);
-btnPasse.addEventListener("click",passe,false);
+btnPasse.addEventListener("click",joueurSuivant,false);
 
 function init() {
+	
 	score1.innerHTML=0;
 	score2.innerHTML=0;
 	tempScore1.innerHTML=0;
@@ -28,6 +30,9 @@ function init() {
 	image.style.display="none";
 	activeJ1.classList.add("actif");
 	activeJ2.classList.remove("actif");
+
+	btnLancer.style.disabled =false;
+	btnPasse.style.disabled =false;
 }
 
 function lancer(){
@@ -35,20 +40,68 @@ function lancer(){
 	image.style.display="inline";
 	image.setAttribute('src',`de-${de}.png`);
 	console.log(parseInt(tempScore1.innerHTML));
-	
+
 
 	if(joueurActif==0){
-		var xx = parseInt(tempScore1.innerHTML);
-		xx+=de;
-		tempScore1.innerHTML = xx;
+		if(de!=1){
+			var xx = parseInt(tempScore1.innerHTML);
+			xx+=de;
+			tempScore1.innerHTML = xx;
+			btnPasse.addEventListener("click",function(){score1.innerHTML=xx;},false);
+			if(score1>=SCORE_MAX){
+				btnLancer.disabled =true;
+				btnPasse.disabled =true;
+			}
+			
+		}
+		else{
+			tempScore1.innerHTML = 0;
+			joueurSuivant();
+		}
+			
+		
 	}
 	else{
-		var xx = parseInt(tempScore2.innerHTML);
-		xx+=de;
-		tempScore2.innerHTML = xx;
+		if(de!=1){
+			var xx = parseInt(tempScore2.innerHTML);
+			xx+=de;
+			tempScore2.innerHTML = xx;
+			
+		}
+		else{
+			tempScore2.innerHTML = 0;
+			joueurSuivant();
+		}
+		
 	}
 }
 
 function passe(){
 
+	if(joueurActif==0){
+		joueurActif=1;
+	}
+	else{
+		joueurActif=0;
+	}
+
 }
+function joueurSuivant(){
+	image.style.display="none";
+	tempScore1.innerHTML = 0;
+	console.log(joueurActif);
+	passe();
+	console.log(joueurActif);
+}
+
+
+var smax;
+if(score1>score2){smax = score1;}
+else{smax = score2;	}
+
+
+var text = document.createTextNode(`lmax score: ${smax}`);
+activeJ2.appendChild(text);
+
+
+
